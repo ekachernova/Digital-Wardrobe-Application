4502c78840230948ec5d63852cc7edbc0a4b37cd
Ekaterina Chernova <chernova.berlin@gmail.com>
2023-05-12 11:51:17 +0200

    add basic jsx structure of the homepage

--- app/page.js
+++ app/page.js
@@ -1,12 +1,38 @@
-import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.center}>
-        <h1>welcome to my application</h1>
-      </div>
-    </main>
+    <>
+      <header className={styles.header}>
+        <div className={styles.logo}>
+          <h2>GETYOURWARDROBE .</h2>
+        </div>
+        <div className={styles.menu}>
+          <ul>
+            <li className={styles.menuItem}>Home</li>
+            <li className={styles.menuItem}>How it works</li>
+            <li className={styles.menuItem}>Blog</li>
+          </ul>
+        </div>
+      </header>
+      <main className={styles.main}>
+        <div>
+          <p>IMAGE SECTION</p>
+        </div>
+        <div>
+          <button className={styles.button}>
+            create your digital wardrobe!
+          </button>
+        </div>
+      </main>
+      <footer className={styles.footer}>
+        <div className={styles.copyright}>
+          <p>COPYRIGHT SECTION</p>
+        </div>
+        <div>
+          <p>Contacts and quick menu section</p>
+        </div>
+      </footer>
+    </>
   );
 }
